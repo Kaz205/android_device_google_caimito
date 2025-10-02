5e1ca4243ad3b6af5f520405a76cc6d88cd6de14
Kazuki H <kazukih0205@gmail.com>
2025-10-02 17:44:13 +0900

    Statix-ify

--- statix_tokay.mk
+++ statix_tokay.mk
@@ -6,20 +6,26 @@
 
 # Inherit some common stuff
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
+# Parts
+$(call inherit-product, vendor/google/pixelparts/pixelparts.mk)
+$(call inherit-product, vendor/google/pixelparts/face/face.mk)
+$(call inherit-product, vendor/google/pixelparts/powershare/device.mk)
 
 # Inherit device configuration
 DEVICE_CODENAME := tokay
 DEVICE_PATH := device/google/caimito
 VENDOR_PATH := vendor/google/tokay
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
-$(call inherit-product, device/google/zumapro/lineage_common.mk)
-$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
+$(call inherit-product, device/google/zumapro/statix_common.mk)
+$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-statix.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 9
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_NAME := statix_$(DEVICE_CODENAME)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2424
@@ -29,5 +35,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="tokay-user 16 BP3A.251105.015 14339231 release-keys" \
     BuildFingerprint=google/tokay/tokay:16/BP3A.251105.015/14339231:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
+
+INCLUDE_PIXEL_LAUNCHER := true
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
